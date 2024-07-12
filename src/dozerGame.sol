8ac268f24dc9468c675db291e3240f50d3f13487
degenRobot <86513395+degenRobot@users.noreply.github.com>
2024-07-13 00:37:43 +0200

    basic logic

---
+++ src/dozerGame.sol
@@ -0,0 +1,103 @@
+// SPDX-License-Identifier: AGPL-3.0
+pragma solidity 0.8.18;
+
+import {ERC20} from "@tokenized-strategy/BaseStrategy.sol";
+
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+interface IOracle {
+    function getValue(address _token, uint256 _amount) external view returns(uint256);
+}
+
+interface IPrizeDetails {
+    function getResults(uint256 _epochNumber) external view returns(uint256[] memory, address[] memory);
+}
+
+contract dozerGame is ERC721 {
+
+    using SafeERC20 for ERC20;
+
+
+    // EPOCH Specific mappings (store relevant info for that epoch for tracking winnings)
+    mapping(uint256 => uint256[]) public resultsAmounts;
+    mapping(uint256 => address[]) public resultsTokens;
+    mapping(uint256 => uint256) public epochEntrants;
+
+    // TOKEN Specific mappings (i.e. tracking deposits / epochs for token ID)
+    // We map token ID to epoch number to track when prize can be claimed 
+    mapping(uint256 => uint256) public epochs;
+    mapping(uint256 => uint256) public entranceValue;
+    IPrizeDetails public prizeDetails;
+    uint256 public epochNumber;
+    uint256 public tokenId;
+    uint256 public minAmount = 10000;
+
+    uint256 public entrantCounter;
+
+    constructor(
+        string memory _name, 
+        string memory _symbol,
+        address _prizeDetails
+    ) ERC721(_name, _symbol) {
+        prizeDetails = IPrizeDetails(_prizeDetails);
+    }
+
+
+    function _completeEpoch() internal {
+        uint256[] memory amounts;
+        address[] memory tokens;
+        
+        (amounts, tokens) = prizeDetails.getResults(epochNumber);
+
+        resultsAmounts[epochNumber] = amounts;
+        resultsTokens[epochNumber] = tokens;
+
+        epochEntrants[epochNumber] = entrantCounter;
+
+        entrantCounter = 0;
+        epochNumber += 1;
+    }
+
+    function deposit(address _coin, uint256 _amount) external {
+
+        // TO DO - this should call Oracle & get "USD" value 
+        uint256 depositValue = _amount; 
+
+        require(depositValue >= minAmount, "not enough deposited");
+
+        ERC20(_coin).transferFrom(msg.sender, address(this), _amount);
+        _mint(msg.sender, tokenId);
+        epochs[tokenId] = epochNumber;
+        entranceValue[tokenId] = depositValue;
+        entrantCounter += depositValue;
+
+        tokenId += 1;
+    } 
+
+    function claimWinning(uint256 _tokenId) external {
+        require(epochNumber > epochs[_tokenId], "Epoch Not Finished Yet");
+        require(ownerOf(_tokenId) == msg.sender, "Not Owner");
+    
+        
+        uint256 _epoch = epochs[_tokenId];
+
+        uint256 a = entranceValue[_tokenId];
+        uint256 b = epochEntrants[_epoch];
+
+        // LOOP Through prizes and transfer to winner
+        uint256[] memory amounts = resultsAmounts[_epoch];
+        address[] memory tokens = resultsTokens[_epoch];
+
+        for (uint256 i = 0; i < amounts.length; i++) {
+
+            uint256 prizeAmount = amounts[i] * a / b;
+            ERC20(tokens[i]).safeTransfer(msg.sender, prizeAmount);
+        }
+
+        _burn(_tokenId);
+
+    }
+
+
+}
--- src/test/utils/Setup.sol
+++ src/test/utils/Setup.sol
@@ -4,24 +4,13 @@ pragma solidity 0.8.18;
 import "forge-std/console.sol";
 import {ExtendedTest} from "./ExtendedTest.sol";
 
-import {Strategy, ERC20} from "../../Strategy.sol";
-import {IStrategyInterface} from "../../interfaces/IStrategyInterface.sol";
+import {dozerGame, ERC20} from "../../dozerGame.sol";
+ 
 
-// Inherit the events so they can be checked if desired.
-import {IEvents} from "@tokenized-strategy/interfaces/IEvents.sol";
-
-interface IFactory {
-    function governance() external view returns (address);
-
-    function set_protocol_fee_bps(uint16) external;
-
-    function set_protocol_fee_recipient(address) external;
-}
-
-contract Setup is ExtendedTest, IEvents {
+contract Setup is ExtendedTest {
     // Contract instances that we will use repeatedly.
     ERC20 public asset;
-    IStrategyInterface public strategy;
+    dozerGame public game;
 
     mapping(string => address) public tokenAddrs;
 
@@ -29,10 +18,8 @@ contract Setup is ExtendedTest, IEvents {
     address public user = address(10);
     address public keeper = address(4);
     address public management = address(1);
-    address public performanceFeeRecipient = address(3);
 
     // Address of the real deployed Factory
-    address public factory;
 
     // Integer variables that will be used repeatedly.
     uint256 public decimals;
@@ -43,7 +30,6 @@ contract Setup is ExtendedTest, IEvents {
     uint256 public minFuzzAmount = 10_000;
 
     // Default profit max unlock time is set for 10 days
-    uint256 public profitMaxUnlockTime = 10 days;
 
     function setUp() public virtual {
         _setTokenAddrs();
@@ -54,97 +40,40 @@ contract Setup is ExtendedTest, IEvents {
         // Set decimals
         decimals = asset.decimals();
 
-        // Deploy strategy and set variables
-        strategy = IStrategyInterface(setUpStrategy());
+        // Deploy the game 
+        game = new dozerGame("Dozer Game", "DG", address(this));
 
-        factory = strategy.FACTORY();
 
         // label all the used addresses for traces
         vm.label(keeper, "keeper");
-        vm.label(factory, "factory");
         vm.label(address(asset), "asset");
         vm.label(management, "management");
-        vm.label(address(strategy), "strategy");
-        vm.label(performanceFeeRecipient, "performanceFeeRecipient");
     }
 
-    function setUpStrategy() public returns (address) {
-        // we save the strategy as a IStrategyInterface to give it the needed interface
-        IStrategyInterface _strategy = IStrategyInterface(
-            address(new Strategy(address(asset), "Tokenized Strategy"))
-        );
 
-        // set keeper
-        _strategy.setKeeper(keeper);
-        // set treasury
-        _strategy.setPerformanceFeeRecipient(performanceFeeRecipient);
-        // set management of the strategy
-        _strategy.setPendingManagement(management);
 
-        vm.prank(management);
-        _strategy.acceptManagement();
 
-        return address(_strategy);
-    }
-
-    function depositIntoStrategy(
-        IStrategyInterface _strategy,
+    function mintAndDepositIntoGame(
         address _user,
-        uint256 _amount
-    ) public {
-        vm.prank(_user);
-        asset.approve(address(_strategy), _amount);
-
-        vm.prank(_user);
-        _strategy.deposit(_amount, _user);
-    }
-
-    function mintAndDepositIntoStrategy(
-        IStrategyInterface _strategy,
-        address _user,
+        address _asset,
         uint256 _amount
     ) public {
         airdrop(asset, _user, _amount);
-        depositIntoStrategy(_strategy, _user, _amount);
+        
+        vm.prank(_user);
+        ERC20(_asset).approve(address(game), _amount);
+
+        vm.prank(_user);
+        game.deposit(_asset, _amount);
+
     }
 
-    // For checking the amounts in the strategy
-    function checkStrategyTotals(
-        IStrategyInterface _strategy,
-        uint256 _totalAssets,
-        uint256 _totalDebt,
-        uint256 _totalIdle
-    ) public {
-        uint256 _assets = _strategy.totalAssets();
-        uint256 _balance = ERC20(_strategy.asset()).balanceOf(
-            address(_strategy)
-        );
-        uint256 _idle = _balance > _assets ? _assets : _balance;
-        uint256 _debt = _assets - _idle;
-        assertEq(_assets, _totalAssets, "!totalAssets");
-        assertEq(_debt, _totalDebt, "!totalDebt");
-        assertEq(_idle, _totalIdle, "!totalIdle");
-        assertEq(_totalAssets, _totalDebt + _totalIdle, "!Added");
-    }
 
     function airdrop(ERC20 _asset, address _to, uint256 _amount) public {
         uint256 balanceBefore = _asset.balanceOf(_to);
         deal(address(_asset), _to, balanceBefore + _amount);
     }
 
-    function setFees(uint16 _protocolFee, uint16 _performanceFee) public {
-        address gov = IFactory(factory).governance();
-
-        // Need to make sure there is a protocol fee recipient to set the fee.
-        vm.prank(gov);
-        IFactory(factory).set_protocol_fee_recipient(gov);
-
-        vm.prank(gov);
-        IFactory(factory).set_protocol_fee_bps(_protocolFee);
-
-        vm.prank(management);
-        strategy.setPerformanceFee(_performanceFee);
-    }
 
     function _setTokenAddrs() internal {
         tokenAddrs["WBTC"] = 0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599;
